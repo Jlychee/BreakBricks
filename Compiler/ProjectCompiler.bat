98a2e1e0e9e6c58d48038df7e6673f1b5e28fc06
Jlychee <2006tka@gmail.com>
2025-04-01 00:54:35 +0500

    че-то сломала

--- Compiler/ProjectCompiler.bat
+++ Compiler/ProjectCompiler.bat
@@ -1,8 +1,5 @@
 @echo off
 
-rem  $Id: JackCompiler.bat,v 1.2 2014/05/10 00:52:43 marka Exp $
-rem  mark.armbrust@pobox.com
-
 setlocal
 
 rem
@@ -10,18 +7,13 @@ set "INPUT_DIR=../src"
 set "OUTPUT_DIR=../vm"
 
 rem
-if not "%2"=="" goto :USAGE
+if not "%1"=="" goto :USAGE
 if "%~1"=="/?" (
 :USAGE
   echo Usage:
-  echo     JackCompiler               Compiles all .jack files in the current
-  echo                                 working directory.
-  echo     JackCompiler DIRECTORY     Compiles all .jack files in DIRECTORY.
-  echo     JackCompiler FILE.jack     Compiles FILE.jack to FILE.vm.
+  echo     ProjectCompiler               compiles jack game files into vm files to run in vmEmulator
   exit /b
 )
-
-echo Compiling "%INPUT_DIR%"
 
 java -classpath "%CLASSPATH%;bin/classes;bin/lib/Hack.jar;bin/lib/Compilers.jar" ^
   Hack.Compiler.JackCompiler "%INPUT_DIR%"
@@ -34,4 +26,6 @@ move "%INPUT_DIR%\*.vm" "%OUTPUT_DIR%" >nul 2>&1
 
 if %ERRORLEVEL% NEQ 0 (
     echo Error moving files. Check paths.
+) else (
+	echo files compiled successfully
 )
